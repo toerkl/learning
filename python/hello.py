c900e923e36624757d92986f0e1cf09a14dd00b1
Tobias Lindqvist <tobias.lindqvist@volvo.com>
2021-06-07 15:44:54 +0200

    Just added some examples from the book

--- python/hello.py
+++ python/hello.py
@@ -1,1 +1,26 @@
-print("Hello World")
+import math
+
+# Say hello
+print(hello)
+# This is a Python comment
+# This variable contains an integer
+quantity = 14
+# This variable contains a float
+unit_price = 26.99
+# This variable contains the result of multiplying quantity times unit price
+extended_price = quantity * unit_price
+# Show the results
+print(extended_price)
+
+# abs function
+x = -4
+y = abs(x)
+print(x)
+print(y)
+# hex function
+z = hex(y)
+print(z)
+# math.sqrt function
+print(math.sqrt(y))
+# pi function
+print(math.pi)
