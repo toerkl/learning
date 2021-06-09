6f80b5940f1a0c888fb852b672539050854f20d6
Tobias Lindqvist <tobias.lindqvist@volvo.com>
2021-06-09 14:55:10 +0200

    Convert decimal numbers to other number systems

---
+++ python/convert_dec_numbers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+x=255
+# covert decimal to other number systems
+x_bin = bin(x)
+x_oct = oct(x)
+x_hex = hex(x)
+
+print(x_bin)
+print(x_oct)
+print(x_hex)
+
+# Show number in decimal number system (no conversion required)
+print(0b11111111)
+print(0o377)
+print(0xff)
