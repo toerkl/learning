6606f8226346ebe7cafd4c3660f211ef67faa9b2
Tobias Lindqvist <tobias.lindqvist@volvo.com>
2021-09-01 11:03:22 +0200

    Add lists

---
+++ python/lists/in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+titleist_golf_balls = ["prov1", "prov1x", "nxt", "nxt_tour"]
+
+# Is avx in list?
+has_avx = "avt" in titleist_golf_balls
+print(has_avx)
+
+# Is prov1x in list?
+has_prov1x = "prov1x" in titleist_golf_balls
+print(has_prov1x)
