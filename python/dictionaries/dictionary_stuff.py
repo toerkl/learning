c7a08d433e747c0d9ca6f7a95cb5b5b9e928dd0a
Tobias Lindqvist <tobias.lindqvist@volvo.com>
2021-09-20 22:44:02 +0200

    Dictionaries

---
+++ python/dictionaries/dictionary_stuff.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+people = {
+    'tlindqvist': 'Tobias Lindqvist',
+    'sjohansson': 'Sven Johansson',
+    'bsvensson': 'Bosse Svensson',
+    'nbertilsson': 'Nils Bertilsson'
+}
+
+# Print tlindqvist
+print(people['tlindqvist'])
+
+# Put person in a variable
+person = 'tlindqvist'
+print(people[person])
+
+# Get length of dictionary
+print(len(people))
+
+# Check if key exists in dictionary
+persons = ['tlindqvist', 'bvonfjong']
+for person in persons:
+    if (person in people):
+        print(f"Person {people[person]} with key {person} is in dictionary.")
+    else:
+        print(f"Person with key {person} does not is in dictionary")
