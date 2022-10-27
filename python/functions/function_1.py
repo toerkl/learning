2fa333a7bc73b6aebd6958689c832746f1f760b8
Tobias Lindqvist <tobias.lindqvist@ecarxgroup.com>
2022-10-27 22:53:39 +0200

    Add kwargs

--- python/functions/function_1.py
+++ python/functions/function_1.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python
 
-def hello:  # Practice function
+def hello(user_name):  # Practice function
     """" A docstring describing the function """
-    print('Hello')
+    print(f'Hello {user_name}')
+
+
+def hello_default(user_name='nobody'):
+    """" A function with a default parameter """
+    print(f'Hello {user_name}')
+
+
+def hello_first_last_number(
+    first, last, number=666
+):
+    """ A function with a default parameter
+        Default arg has to be last """
+
+    print(f'Hello {first} {last}')
+    print(f'The number is {number}')
+
+
+hello('tobbe')
+hello_default()
+hello_first_last_number('nisse', 'larsson')
+
+# Keywork arguments
+hello_first_last_number(number=123, last='Svensson', first='Bosse')
