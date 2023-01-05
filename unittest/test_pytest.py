6bcd8ed7ab9e1a89fb83f91050663ca3eff4b9d3
toerkl <tlindqvist@gmail.com>
2023-01-05 20:39:04 +0100

    Add unittests and pytests

---
+++ unittest/test_pytest.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import inc_dec   # The code to test
+
+def test_increment():
+    assert inc_dec.increment(3) == 4
+
+
+def test_decrement():
+    assert inc_dec.decrement(3) == 4
